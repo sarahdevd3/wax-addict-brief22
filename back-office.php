989a6d7f724cd7564ce6304d0fa06c7add552146
sarahdevd3 <74182942+sarahdevd3@users.noreply.github.com>
2020-12-05 14:03:19 +0100

    Update back-office.php

--- back-office.php
+++ back-office.php
@@ -1,8 +1,18 @@
 <?php // lancement d'une session
-include_once("sessioninit.php");
+
+    include_once("session-init.php");
+
 // vérification de la session
+
 if (isset($_SESSION["user"]) AND is_array($_SESSION["user"])) {
 // yes, alors bienvenue
-include("backofficetemplate.php"); } else { // bye
-header("Location: index.html#mauvaise-session"); exit; // exit après une redirection
+    include("back-office--template.php");
+ } 
+
+else { 
+     // bye
+header("Location: index.html#mauvaise-session");
+
+exit; 
+// exit après une redirection
 } ?>
